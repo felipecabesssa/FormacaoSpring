4dad97eb0a1b4674c2867fc8f0935b28c08e281b
felipecabesssa <felipesoares100@hotmail.com>
2021-11-21 22:29:59 -0300

    Operacoes CRUD

---
+++ Modulos/2 - Do JDBC ao SpringData/cursos/4 - Spring Data JPA - Repositorios - Consultas - Projecoes e Specifications/spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -0,0 +1,94 @@
+package br.com.alura.spring.data.orm;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private Double salario;
+	private LocalDate dataContratacao;
+	
+	@ManyToOne
+	@JoinColumn(name = "cargo_id", nullable = false)
+	private Cargo cargo;
+	
+	@Fetch(FetchMode.SELECT)
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "funcionarios_unidades", joinColumns = {
+			@JoinColumn(name = "fk_funcionario") },
+	inverseJoinColumns = { @JoinColumn(name = "fk_unidade") })
+	private List<UnidadeTrabalho> unidadeTrabalhos;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public LocalDate getDataContratacao() {
+		return dataContratacao;
+	}
+
+	public void setDataContratacao(LocalDate dataContratacao) {
+		this.dataContratacao = dataContratacao;
+	}
+
+	public void setUnidadeTrabalhos(List<UnidadeTrabalho> unidades) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCargo(Cargo cargo2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
